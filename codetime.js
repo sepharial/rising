17425d20699ae5d8358762fc899f7d52cc8a61bf
Mia <mjf22@st-andrews.ac.uk>
2018-07-26 15:06:01 -0400

    Deleted script tags

--- codetime.js
+++ codetime.js
@@ -1,21 +1,19 @@
-<script>
 (function($){$(document).ready(function(){
     $("[title]").style_my_tooltips({
     tip_follows_cursor:true,
     tip_delay_time:30,
     tip_fade_speed:300,
     attribute:"title"});});})(jQuery);
-</script>
 
-<script>
+
     $(document).ready(function(){
     	$(".lineage-content").hide();
         $(".clicky").click(function(){
             $(this).next(".lineage-content").slideToggle("750");
 		});
     });
-</script>
-<script>
+
+
     $(document).ready(function(){
         $(".menu").click(function(){
             $(".usercontrols").slideToggle("750");
@@ -25,4 +23,3 @@
             $(".usercontrols, .clickypanel").slideUp("750");
 		});
     });
-</script>
